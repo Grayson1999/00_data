5cf57d857f962ae17479e4b9d1b782408663f0d5
Grayson1999 <bbt1250912@gmail.com>
2022-06-29 16:05:32 +0900

    add requirements

--- acin_clawler/practice.py
+++
@@ -1,9 +0,0 @@
-from comment import arrangement
-
-company = "naver"
-search = "한동훈"
-start_date = "20220620"
-end_date = "20220623"
-date = "20220620"
-
-arrangement.Comment(f'./result/{company}_news/news_{search}_{company}_{start_date}_{end_date}__{date[:6]}.json')
